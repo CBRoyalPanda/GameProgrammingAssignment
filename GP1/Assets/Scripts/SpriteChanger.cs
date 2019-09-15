03a8d2a17cd6897aac956c4c087da5ea86e28251
CBRoyalPanda <50720170+CBRoyalPanda@users.noreply.github.com>
2019-09-15 16:43:42 -0700

    I added my existing unity project
    Should have all of the basics

---
+++ GP1/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer theRenderer;
+    public Color spriteColor;
+
+    void Start()
+    {
+        theRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteColor.a = 1.0f;
+
+        if (theRenderer != null)
+        {
+            theRenderer.color = spriteColor;
+        }
+        theRenderer.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
