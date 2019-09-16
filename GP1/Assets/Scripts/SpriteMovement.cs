f812420cb33faaaca3b60f00d0ba163110ed9a75
CBRoyalPanda <50720170+CBRoyalPanda@users.noreply.github.com>
2019-09-16 12:20:08 -0700

    Added second move option
    Added a more acurate movement system

---
+++ GP1/Assets/Scripts/SpriteMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    private Transform tf;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myVector = new Vector3(2,4,12);
+        myVector = myVector.normalized;
+        tf.position = tf.position + (myVector * speed);
+    }
+}
