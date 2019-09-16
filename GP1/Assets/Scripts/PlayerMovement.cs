d15068841834e2620ca362d5f4cc68751a0bb5f9
CBRoyalPanda <50720170+CBRoyalPanda@users.noreply.github.com>
2019-09-15 17:14:02 -0700

    Added basic player movement
    Press D and the sprite moves to the right

---
+++ GP1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    // This is the value that determines the speed of the ship
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Whenever the player uses the D key on the keyboard...
+        if (Input.GetKey(KeyCode.D))
+        {
+            // ... then the spite will move to the right
+            transform.Translate(Vector2.right*speed*Time.deltaTime);
+        }
+    }
+}
